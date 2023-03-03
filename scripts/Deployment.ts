658c52f4b39cfe1e026404be908019f37ed74749
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-03 06:11:54 -0700

    Configured env

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -1,4 +1,6 @@
-import { ethers } from 'hardhat';
+import { ethers, Wallet } from 'ethers';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 function convertStringArrayToBytes32(array: string[]) {
   const bytes32Array = [];
@@ -13,21 +15,28 @@ async function main () {
   const proposals = args.slice(2);
   if (proposals.length <= 0) throw new Error("Missing parameters: proposals");
 
-  const provider = ethers.provider;
+  const provider = ethers.getDefaultProvider("goerli");
+
+  const wallet = ethers.Wallet.createRandom();
+  console.log(`Connected to the wallet address ${wallet.address}`);
+
+  const signer = wallet.connect(provider);
+  const balance = await signer.getBalance();
+  console.log(`Wallet balance: ${balance} Wei`);
 
   return;
-  console.log("Deploying Ballot contract!");
-  console.log("Proposals: ");
-  proposals.forEach((element, index) => {
-    console.log(`Proposal N. ${index + 1}: ${element}`);
-  });
-  const ballotContractFactory = await ethers.getContractFactory("Ballot");
-  const ballotContract = await ballotContractFactory.deploy(
-      convertStringArrayToBytes32(proposals)
-  );
-  console.log("Deploying contract ...");
-  await ballotContract.deployTransaction.wait();
-  console.log(`The Ballot contract was deployed at the address ${ballotContract.address}`)
+  // console.log("Deploying Ballot contract!");
+  // console.log("Proposals: ");
+  // proposals.forEach((element, index) => {
+  //   console.log(`Proposal N. ${index + 1}: ${element}`);
+  // });
+  // const ballotContractFactory = await ethers.getContractFactory("Ballot");
+  // const ballotContract = await ballotContractFactory.deploy(
+  //     convertStringArrayToBytes32(proposals)
+  // );
+  // console.log("Deploying contract ...");
+  // await ballotContract.deployTransaction.wait();
+  // console.log(`The Ballot contract was deployed at the address ${ballotContract.address}`)
 }
 
 main().catch((error) => {
